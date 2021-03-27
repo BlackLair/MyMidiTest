88b178f6f69b266cf10df59a269815354e614547
JW <kiw970923@naver.com>
2021-03-27 12:05:02 +0900

    콜백 클래스 별도 생성/건반 입력 받는 즉시 수신된 MIDI데이터 출력가능/디바이스 콜백 해제 버튼 생성

--- app/src/main/java/com/example/mymidi/MainActivity.java
+++ app/src/main/java/com/example/mymidi/MainActivity.java
@@ -27,98 +27,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     static final String TAG = "mymidi";
-    MidiManager m;
+    static MidiManager m;
     MidiOutputPort outputPort;
     TextView myData, tv_byte;
-    Button btn;     //임시 버튼
-
-    int testValue=0;
+    Button btn, btn2;     //임시 버튼
+    public static Context context_main;
 
 
     static SoundPool spools;  // spool은 건반 1개당 1개씩 필요하므로 실제론 88개가 필요함.<배열로 정의 가능
     static int keys[];          // spool에 할당된 오디오 파일을 구분할 키값
-    static Context context;
+
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myData=(TextView)findViewById(R.id.myData);
-   //     btn=(Button)findViewById(R.id.btn);             // 임시_누를 때마다 마지막 신호를 보여줄 버튼
+
         tv_byte=(TextView)findViewById(R.id.tv_byte);       //임시_미디 신호에서 파싱한 값을 보여줄 텍스트뷰
         m=(MidiManager)getApplicationContext().getSystemService(Context.MIDI_SERVICE);
         MidiDeviceInfo[] infos=m.getDevices();
-        context=getApplicationContext();
+        context_main=getApplicationContext();
         btn=findViewById(R.id.btn);
-
-
-
+        btn2=findViewById(R.id.btn_unregister);
+        context_main=this;
 
         keys= new int[88];
 
-        spools=SoundSelector.load(keys,0,context );
+        spools=SoundSelector.load(keys,0,context_main );
 
+        MyDeviceCallback myMidiCallback = new MyDeviceCallback(this); // 콜백 클래스 생성
+        m.registerDeviceCallback(myMidiCallback,Handler.createAsync(Looper.getMainLooper()) ); // 장치 연결시작
 
-        m.registerDeviceCallback(new MidiManager.DeviceCallback() {      // 실시간으로 장치가 연결되거나 해제되면 알림
-            public void onDeviceAdded( MidiDeviceInfo info ) {
-                Bundle properties = info.getProperties();       // 연결된 장치의 정보를 저장할 멤버
-                String manufacturer = properties.getString(MidiDeviceInfo.PROPERTY_NAME);   // 연결된 장치의 이름을 가져옴
-                Toast.makeText(getApplicationContext(),"device connected:"+manufacturer, Toast.LENGTH_SHORT).show();    //장치가 연결되었을 때 연결된 장치이름을 알림
-                m.openDevice(info, new MidiManager.OnDeviceOpenedListener(){        //장치 열기
-                            @Override
-                            public void onDeviceOpened(MidiDevice device){  // 장치와의 통신이 열렸을 때 수행
-                                if(device==null){
-                                    Log.e(TAG, "Could not open device"+info);
-                                }else{
-
-                                    outputPort=device.openOutputPort(0);    // 미디 아웃 포트 열기
-                                    outputPort.connect(new MyReceiver());       // 포트로 리시버 연결
-                                    new Thread(new Runnable(){/////////////////////(중요) 쓰레드 내에서 UI 작업하는 기능 !!!!!!!!!
-                                        @Override
-                                        public void run() {
-                                            runOnUiThread(new Runnable(){
-                                                @Override
-                                                public void run(){
-                                                    btn.setText("Device connected");
-                                                }
-                                            });
-                                        }
-                                    }).start();  ///////////////////////////////////////////////////////////////////////
-
-                                }
-                            }
-                        }, new Handler(Looper.getMainLooper())  // 장치가 연결된 동안 항상 동작하도록 쓰레드 작동
-                );
-
-            }
-            public void onDeviceRemoved( MidiDeviceInfo info ) { // 장치 연결 해제 알림
-                Toast.makeText(getApplicationContext(),"device removed", Toast.LENGTH_SHORT).show();
-
-               new Thread(new Runnable(){/////////////////////(중요) 쓰레드 내에서 UI 작업하는 기능 !!!!!!!!!
-                    @Override
-                    public void run() {
-                        runOnUiThread(new Runnable(){
-                            @Override
-                            public void run(){
-                                btn.setText("Device Disconnected : "+testValue);
-                                testValue+=1;
-                            }
-                        });
-                    }
-                }).start();  ///////////////////////////////////////////////////////////////////////
-
-            }
-        }, Handler.createAsync(Looper.getMainLooper()));  // createAsync는 API 28 필요
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {   // 테스트 코드.  버튼을 누르면 마지막으로 받은 미디 신호를 hex코드로 출력(정렬안됨)
-
                 tv_byte.setText(CheckData.testString);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context_main, "디바이스 콜백 해제", Toast.LENGTH_SHORT).show();
+                m.unregisterDeviceCallback(myMidiCallback); // 디바이스 콜백 해제
             }
         });
     }
     public static Context getAppContext(){
-        return MainActivity.context;
+        return MainActivity.context_main;
     }
 
 }
--- app/src/main/java/com/example/mymidi/MyReceiver.java
+++ app/src/main/java/com/example/mymidi/MyReceiver.java
@@ -1,24 +1,41 @@
 package com.example.mymidi;
 
+import android.app.Activity;
 import android.media.midi.MidiReceiver;
-import android.view.View;
+import android.widget.TextView;
 
 import java.io.IOException;
-
 
 import static com.example.mymidi.MainActivity.spools;
 
 public class MyReceiver extends MidiReceiver {
-
+    private Activity activity;
+    TextView myText;
+    public MyReceiver(Activity activity){
+        super();
+        this.activity=activity;
+    }
     public void onSend(byte[] data, int offset, int count, long timestamp) throws IOException {
         StringBuilder sb = new StringBuilder();
+        myText=activity.findViewById(R.id.myData);
         for (byte b : data) {         // data의 값들을 하나씩 b에 대입하여 반복하는 문법
             sb.append(String.format("%02x", b & 0xff));           // 받은 데이터를 헥스 문자열로 변환
             if(sb.toString().length()>=20)    // 필요한 부분만 남기고 다음 메시지는 걸러 레이턴시 크게 감소
                 break;
         }
         String receivedDataString = sb.toString();//.substring(0, 20);       // 문자열 뒤쪽 필요없는 데이터 거름
-
+        new Thread(new Runnable(){/////////////////////(중요) 쓰레드 내에서 UI 작업하는 기능 !!!!!!!!!
+            @Override
+            public void run() {
+                activity.runOnUiThread(new Runnable(){
+                    @Override
+                    public void run(){
+                        myText.setText(receivedDataString);
+                    }
+                });
+            }
+        }).start();
         CheckData.CheckNote(receivedDataString, spools, MainActivity.keys);
     }
+
 }
