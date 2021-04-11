e7f262cba3dd8198eca7b88c5b925595dc839732
JW <kiw970923@naver.com>
2021-04-12 00:11:20 +0900

    쓰레드 연주 도중 구현

--- app/src/main/java/com/example/mymidi/MainActivity.java
+++ app/src/main/java/com/example/mymidi/MainActivity.java
@@ -23,7 +23,7 @@ import android.widget.Toast;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -33,11 +33,12 @@ public class MainActivity extends AppCompatActivity {
     TextView myData, tv_byte;
     Button btn, btn2;     //임시 버튼
     public static Context context_main;
-
+    Button stopthread;
 
     static SoundPool spools;  // spool은 건반 1개당 1개씩 필요하므로 실제론 88개가 필요함.<배열로 정의 가능
     static int keys[];          // spool에 할당된 오디오 파일을 구분할 키값
-
+    AtomicBoolean flag=new AtomicBoolean();
+    Thread a;
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,17 +55,42 @@ public class MainActivity extends AppCompatActivity {
         context_main=this;
         keys= new int[88];
 
+        flag.set(true);
         spools=SoundSelector.load(keys,0,context_main );
-
+        stopthread=findViewById(R.id.stopthread);
+        stopthread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flag.set(false);
+            }
+        });
         MyDeviceCallback myMidiCallback = new MyDeviceCallback(this); // 콜백 클래스 생성
         m.registerDeviceCallback(myMidiCallback,Handler.createAsync(Looper.getMainLooper()) ); // 장치 연결시작
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {   // 테스트 코드.  버튼을 누르면 마지막으로 받은 미디 신호를 hex코드로 출력(정렬안됨)
-                tv_byte.setText(CheckData.testString);
-            }
-        });
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {   // 테스트 코드.  버튼을 누르면 마지막으로 받은 미디 신호를 hex코드로 출력(정렬안됨)
+                        tv_byte.setText(CheckData.testString);
+                        flag.set(true);
+                        a=new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                while(true){
+                                    int i=spools.play(keys[30],1,1,0,0,1);
+                                    try {
+                                        Thread.sleep(545);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                    spools.stop(i);
+                                    if(flag.get()==false)
+                                        break;
+                                }
+                            }
+                        });
+                        a.start();
+                    }
+                });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,9 +101,17 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, SubActivity.class);
                 startActivity(intent);
                 finish();
+
             }
         });
     }
+
+    @Override
+    protected void onDestroy() {
+        flag.set(false);
+        super.onDestroy();
+    }
+
     public static Context getAppContext(){
         return MainActivity.context_main;
     }
